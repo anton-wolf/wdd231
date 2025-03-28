b947241ba14262f78fee6691c5c259d441708b30
Katlego <kat@geekpop.io>
2025-03-28 18:41:18 +0200

    week two project: added header

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,8 @@
+// output DOM elements
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+// date object
+const today = new Date();
+year.innerHTML = `${today.getFullYear()}`
+lastModified.innerHTML = `Last Modification: ${document.lastModified}`
